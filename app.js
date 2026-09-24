05dad4c67b161dc6683f189c3542b3c218026a8c
Samir <samirmaia1998@gmail.com>
2026-09-24 19:35:40 -0300

    merge resolvido

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('form');
 const input = document.getElementById('newItem');
 const list = document.getElementById('list');
+const title = document.getElementById('title');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -12,12 +13,20 @@ form.addEventListener('submit', (e) => {
 
 function addItem(text) {
   const li = document.createElement('li');
+  li.dataset.text = text;
   li.textContent = text;
   li.addEventListener('click', () => toggleDone(li));
   list.appendChild(li);
+  updateTitle();
 }
 
-// Função alvo de CONFLITO na Aula 2:
 function toggleDone(li) {
   li.classList.toggle('done');
+  li.textContent = li.classList.contains('done')
+    ? `${li.dataset.text} ✅`
+    : li.dataset.text;
+}
+
+function updateTitle() {
+  title.textContent = `taskBoard - equipe a (${list.children.length})`;
 }
